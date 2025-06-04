7aea59ae79e03c862b031ada710dbce60710a1e5
Stanislav Basarab <stanislavbasarab083@gmail.com>
2025-06-04 20:09:29 +0300

    chore: configure linter & prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,55 +1,41 @@
-import nx from "@nx/eslint-plugin";
+import nx from '@nx/eslint-plugin';
 
 export default [
-    ...nx.configs["flat/base"],
-    ...nx.configs["flat/typescript"],
-    ...nx.configs["flat/javascript"],
-    {
-        ignores: [
-            "**/dist"
-        ]
+  // ref: https://github.com/nrwl/nx/tree/master/packages/eslint-plugin/src/flat-configs
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  {
+    ignores: ['**/dist', '**/node_modules'],
+  },
+  // common rules
+  {
+    files: ['**/*.ts', '**/*.js'],
+    rules: {
+      // symbols, indents, quotes
+      'comma-dangle': ['error', 'always-multiline'],
+      'comma-spacing': 'error',
+      'key-spacing': 'error',
+      'keyword-spacing': 'error',
+      'space-before-blocks': 'error',
+      'space-before-function-paren': 'error',
+      'space-in-parens': 'error',
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+
+      // brackets, arrows, curly braces
+      'arrow-parens': ['error', 'always'],
+      'arrow-spacing': 'error',
+      'block-spacing': 'error',
+      'brace-style': 'error',
+      'new-parens': 'error',
+      'object-curly-spacing': ['error', 'always'],
+
+      // other
+      'eol-last': ['error', 'always'],
+      'func-call-spacing': 'error',
+      'rest-spread-spacing': 'error',
+      eqeqeq: ['error', 'always'],
     },
-    {
-        files: [
-            "**/*.ts",
-            "**/*.tsx",
-            "**/*.js",
-            "**/*.jsx"
-        ],
-        rules: {
-            "@nx/enforce-module-boundaries": [
-                "error",
-                {
-                    enforceBuildableLibDependency: true,
-                    allow: [
-                        "^.*/eslint(\\.base)?\\.config\\.[cm]?js$"
-                    ],
-                    depConstraints: [
-                        {
-                            sourceTag: "*",
-                            onlyDependOnLibsWithTags: [
-                                "*"
-                            ]
-                        }
-                    ]
-                }
-            ],
-          "@typescript-eslint/no-empty-object-type": "off",
-          "@typescript-eslint/no-empty-interface": "off",
-        }
-    },
-    {
-        files: [
-            "**/*.ts",
-            "**/*.tsx",
-            "**/*.cts",
-            "**/*.mts",
-            "**/*.js",
-            "**/*.jsx",
-            "**/*.cjs",
-            "**/*.mjs"
-        ],
-        // Override or add rules here
-        rules: {}
-    }
+  },
 ];
