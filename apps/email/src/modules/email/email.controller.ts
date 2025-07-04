3df836d507f70848dc526041c91d536762e58fec
Stanislav Basarab <stanislavbasarab083@gmail.com>
2025-07-05 02:02:37 +0300

    feat: separate layers in email

--- apps/email/src/modules/email/email.controller.ts
+++ apps/email/src/modules/email/email.controller.ts
@@ -1,23 +1,36 @@
 import { GrpcMethod, GrpcService } from '@nestjs/microservices';
-import { EmailService } from './email.service';
 import type {
   Empty,
   IEmailController,
   SendConfirmationRequest,
   SendForecastRequest,
 } from '@types';
+import { ConfirmationDto } from '../dto/confirmation.dto';
+import { ForecastDto } from '../dto/forecast.dto';
+import { Inject } from '@nestjs/common';
+import { EmailDiTokens } from '../constants/di-tokens.const';
+
+export interface IEmailService {
+  sendConfirmation(dto: ConfirmationDto): Promise<void>;
+  sendForecast(dto: ForecastDto): Promise<void>;
+}
 
 @GrpcService()
 export class EmailController implements IEmailController {
-  constructor(private readonly service: EmailService) {}
+  constructor(
+    @Inject(EmailDiTokens.EMAIL_SERVICE)
+    private readonly service: IEmailService
+  ) {}
 
   @GrpcMethod('EmailService', 'SendConfirmation')
-  sendConfirmation(request: SendConfirmationRequest): Promise<Empty> {
-    return this.service.sendConfirmation(request);
+  async sendConfirmation(request: SendConfirmationRequest): Promise<Empty> {
+    await this.service.sendConfirmation(request);
+    return {};
   }
 
   @GrpcMethod('EmailService', 'SendForecast')
-  sendForecast(request: SendForecastRequest): Promise<Empty> {
-    return this.service.sendForecast(request);
+  async sendForecast(request: SendForecastRequest): Promise<Empty> {
+    await this.service.sendForecast(request);
+    return {};
   }
 }
